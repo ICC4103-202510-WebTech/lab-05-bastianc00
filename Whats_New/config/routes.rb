8826fe8c2a9eb13da84900a197b70c38fd9e5fb3
bastianc00 <bcepeda@miuandes.cl>
2025-05-30 03:17:48 -0400

    test_change

--- Whats_New/config/routes.rb
+++ Whats_New/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users, only: [:index, :show, :new, :create, :edit, :update]
   resources :chats, only: [:index, :show, :new, :create, :edit, :update]
   resources :messages, only: [:index, :show, :new, :create, :edit, :update]
