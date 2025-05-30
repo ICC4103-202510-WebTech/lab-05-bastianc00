58af92b860b5a3b8da9fdec4bf414fe2978caa3e
bastianc00 <bcepeda@miuandes.cl>
2025-05-30 02:50:29 -0400

    Update error

--- Whats_New/config/routes.rb
+++ Whats_New/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show, :new, :create, :edit, :update]
-  resources :chats, only: [:index, :show, :new, :create, :edit, :update]
-  resources :messages, only: [:index, :show, :new, :create, :edit, :update]
+  resources :users, only: [:index, :show]
+  resources :chats, only: [:index, :show]
+  resources :messages, only: [:index, :show]
   
   root 'users#index'
 end
